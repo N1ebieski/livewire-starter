9f79e869e1f0df79494455e40ca01fcc3fe4ad28
N1ebieski <kontakt@intelekt.net.pl>
2023-10-03 14:23:30 +0200

    refactoring

--- app/Providers/SpotlightServiceProvider.php
+++ app/Providers/SpotlightServiceProvider.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Livewire;
-use App\Support\Url\UrlHelper;
+use App\Support\Livewire\Url\UrlHelper;
 use Illuminate\Support\ServiceProvider;
 use App\Extends\LivewireUI\Spotlight\Spotlight;
 
--- app/Support/Livewire/Url/UrlHelper.php
+++ app/Support/Livewire/Url/UrlHelper.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Support\Url;
+namespace App\Support\Livewire\Url;
 
 use Illuminate\Support\Str;
 use Livewire\LivewireManager;
