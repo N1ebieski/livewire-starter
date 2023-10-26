cb3c9c036e7f07c05a351d657681c97bf04b4966
N1ebieski <kontakt@intelekt.net.pl>
2023-10-26 11:51:20 +0200

    fix

--- app/Support/Query/Columns/ColumnsHelper.php
+++ app/Support/Query/Columns/ColumnsHelper.php
@@ -11,7 +11,25 @@ final class ColumnsHelper
     public static function getColumnsAsString(array $columns): string
     {
         return (new Collection($columns))
-            ->map(fn ($column) => '`' . $column . '`')
+            ->map(function (string $column) {
+                return self::getColumnWithTicks($column);
+            })
             ->implode(',');
+    }
+
+    public static function getColumnWithTicks(string $column): string
+    {
+        $names = explode('.', $column);
+
+        return (new Collection($names))
+            ->map(function (string $name) {
+                return '`' . $name . '`';
+            })
+            ->implode('.');
+    }
+
+    public static function getColumnWithSnakes(string $column): string
+    {
+        return self::getColumnWithTicks(str_replace('.', '_', $column));
     }
 }
